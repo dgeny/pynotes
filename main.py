30f339332d1fe764efdeae76be67b40660806503
Eugene N <kan1fol@ya.ru>
2023-07-20 15:33:39 +0200

    Code: code runner for presenter added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+from presenter.simple_console_presenter import SimpleConsolePresenter
+
 
 def main():
-    pass
+    presenter = SimpleConsolePresenter()
+    presenter.run()
 
 
 if __name__ == '__main__':
